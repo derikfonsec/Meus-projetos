1967e6bd0c994f389b38f5375ca78b18e931b6e6
derikfonsecx <derikfonseca406@gmail.com>
2024-01-17 17:02:45 -0300

    rename js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,43 @@
 let tarefas = [];
 
-function adicionarTarefa(tarefa) {
-    tarefas.push(tarefa);
-}
+        function adicionarTarefa(tarefa) {
+            tarefas.push({ description: tarefa, done: false });
+            atualizarListaTarefas();
+        }
 
-function removerTarefa(index) {
-    tarefas.splice(index, 1);
-}
+        function removerTarefa(index) {
+            tarefas.splice(index, 1);
+            atualizarListaTarefas();
+        }
 
-function marcarTarefas(index) {
-    tarefas[index].done = true;
-}
+        function marcarTarefas(index) {
+            tarefas[index].done = true;
+            atualizarListaTarefas();
+        }
 
+        function atualizarListaTarefas() {
+            const tarefasDiv = document.querySelector('.tarefas');
+            tarefasDiv.innerHTML = '';
+
+            tarefas.forEach((tarefa, index) => {
+                const tarefaElement = document.createElement('div');
+                tarefaElement.innerHTML = `
+                    <span>${tarefa.description}</span>
+                    <button onclick="removerTarefa(${index})">Remover</button>
+                    <button onclick="marcarTarefas(${index})">Concluir</button>
+                `;
+                if (tarefa.done) {
+                    tarefaElement.classList.add('concluida');
+                }
+                tarefasDiv.appendChild(tarefaElement);
+            });
+        }
+
+        function adicionarComEnter(event) {
+            if (event.key === 'Enter') {
+                adicionarTarefa(document.getElementById('nova-tarefa').value);
+            }
+        }
+
+        // Atualiza a lista de tarefas inicialmente (pode ser removido se desejar)
+        atualizarListaTarefas();
